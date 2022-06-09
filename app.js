956fdba3f1fefc79b6c1bfde15ece0c7c71c811c
B14ck <52348159+blackangiliq@users.noreply.github.com>
2022-06-09 04:40:37 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const os = require('os');
+const https = require('https');
+const internal = require('stream');
+let networkInterfaces = os.networkInterfaces();
+
+
+var minutes = 1, the_interval = minutes * 1 * 1000;
+setInterval(function() {
+
+ try{
+
+let nonLocalInterfaces = {};
+for (let inet in networkInterfaces) {
+  let addresses = networkInterfaces[inet];
+  for (let i=0; i<addresses.length; i++) {
+    let address = addresses[i];
+    if (!address.internal) {
+      if (!nonLocalInterfaces[inet]) {
+        nonLocalInterfaces[inet] = [];
+      }
+      nonLocalInterfaces[inet].push(address);
+    }
+  }
+}
+
+console.log(nonLocalInterfaces['Ethernet 2'][1]['address']);//check if ur interface of openvpn is change by test the code down below
+//console.log(nonLocalInterfaces);
+https.get(`https://username:password@dynupdate.no-ip.com/nic/update?hostname=mytest.example.com&myip==${nonLocalInterfaces['Ethernet 2'][1]['address']}`, (resp) => {
+  let data = '';
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+  resp.on('end', () => {
+    console.log(data);
+  });
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+ }catch(e){
+   console.log(e)
+ }
+  // do your stuff here
+}, the_interval);
